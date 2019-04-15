ecbbdf59aba8fb99b81de4ba36e8aa3b09b6c1d5
phubao1999 <phutuongbao1999@gmail.com>
2019-04-16 00:55:32 +0700

    Finish Myproject 7

--- 7/bai-tap2/index.js
+++ 7/bai-tap2/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    console.log('Alo ALo');
+    //console.log('Alo ALo');
+    $('.nutmora').click(function (e) { 
+        $('.menutrai').toggleClass('traisang');
+    });
+    $('.nutmora2').click(function (e) { 
+        $('.menutren').toggleClass('trenxuong');
+    });
+    $('.nutmora3').click(function (e) { 
+        $('.menutrai').toggleClass('traisang');
+        $('.cacnut').toggleClass('sangtrai');
+    });
 });
