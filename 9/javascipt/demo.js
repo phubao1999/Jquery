a8377c7a09e1712a5c3f8df58d7cf3ebbbc57a57
phubao1999 <phutuongbao1999@gmail.com>
2019-05-03 17:20:41 +0700

    Update Jquery more scrollTop have more effect

--- 9/javascipt/demo.js
+++ 9/javascipt/demo.js
@@ -1,13 +1,25 @@
 $(document).ready(function () {
     $(window).scroll(function () { 
-        // console.log($('html, body').scrollTop()); 650
+        // console.log($('html, body').scrollTop()); 
         var viTri = $('html, body').scrollTop();
-        if( viTri >= 650) {
+        if( (viTri >= 650) && (viTri < 1200)) {
             // console.log('ok');
             $('.menutren').addClass('bienhinh');
         }
         else {
             $('.menutren').removeClass('bienhinh');
+        }
+        if ((viTri) > 1200 && (viTri < 1800)){
+            $('.menutren').addClass('lv1');
+        }
+        else {
+            $('.menutren').removeClass('lv1');
+        }
+        if ((viTri) > 1800 && (viTri < 3000)){
+            $('.menutren').addClass('lv2');
+        }
+        else {
+            $('.menutren').removeClass('lv2');
         }
     });
 });
