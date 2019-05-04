f02ca556bfd938adc6c7114f5284af524e0726ee
phubao1999 <phutuongbao1999@gmail.com>
2019-05-04 13:58:47 +0700

    Update ss2 of project 9

--- 9/2/js/demo.js
+++ 9/2/js/demo.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    console.log('abc');
+    $(window).scroll(function (e) { 
+        var vitriBody = $('html, body').scrollTop();
+        var vitriGiaodien = $('.giaodien').offset().top;
+        vitriGiaodien = vitriGiaodien - 600;
+        
+        if (vitriBody >= vitriGiaodien){
+            // console.log('abc');
+            $('.giaodien').addClass('hienra');
+        }
+        else {
+            $('.giaodien').removeClass('hienra');
+        }
+    });
 });
